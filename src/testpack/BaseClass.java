b87f38c326958b90a2d91c78ca209a088a222787
purna11 <g.purna89@gmail.com>
2018-04-27 10:14:28 +0530

    added logic for BaseClass

--- src/testpack/BaseClass.java
+++ src/testpack/BaseClass.java
@@ -1,9 +1,12 @@
 package testpack;
 
+
+
 public class BaseClass {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		BaseClass b=new BaseClass();
+		b.nagamani();
 
 	}
 	
@@ -11,4 +14,21 @@ public class BaseClass {
 		
 	}
 
+	public void nagamani()
+
+	{
+		String str = "nagamani";
+		 
+		char[] s = str.toCharArray();
+		 
+		for (int i = s.length - 1; i >= 0; i--)
+		{
+		    System.out.print(s[i]);    
+		}
+
+		
+		
+	
+
+	}
 }
