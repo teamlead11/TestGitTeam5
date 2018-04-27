5d41262f800ef942e021d53918d4dc4d738166d2
Prabakaran <prabakaranhope@gmail.com>
2018-04-27 12:17:11 +0530

    initial Commit

--- src/testpack/BaseClass.java
+++ src/testpack/BaseClass.java
@@ -1,6 +1,6 @@
 package testpack;
 
-
+import java.util.Scanner;
 
 public class BaseClass {
 
@@ -30,5 +30,15 @@ public class BaseClass {
 		
 	
 
+	}
+	public static void main(String[] args) {
+		String original,reverse ="";
+		Scanner in=new Scanner(System.in);
+		System.out.println("Enter a string to reverse");
+		original = in.nextLine();
+		int length=original.length();
+		for(int i=length-1;i>=0;i--)
+			reverse=reverse+original.charAt(i);
+		System.out.println("Reverse of Enter a string is:" + reverse);
 	}
 }
