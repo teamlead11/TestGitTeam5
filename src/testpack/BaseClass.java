bbe962e48443c5fd891c5338725544ce94dd3817
bharathivenkat <bharathit01@gmail.com>
2018-04-27 12:31:59 +0530

    added reverse string program under bharathi branch

--- src/testpack/BaseClass.java
+++ src/testpack/BaseClass.java
@@ -1,5 +1,7 @@
 package testpack;
 
+import java.util.Scanner;
+
 public class BaseClass {
 
 	public static void main(String[] args) {
@@ -10,5 +12,14 @@ public class BaseClass {
 	public void ganesh(){
 		
 	}
-
+public void bharathi() {
+	String original,reverse ="";
+	   Scanner s=new Scanner(System.in);
+	   System.out.println("Enter a String to reverse");
+	   original =s.nextLine();
+	   int length=original.length();
+	   for(int i=length-1;i>=0;i--) 
+		   reverse=reverse+original.charAt(i);
+		   System.out.println("Reversed of entered String is:" +reverse);
+}
 }
