fc5e5e668ed9ea68938145056665f7f7df42daa0
saravananscala <saran.scala3@gmail.com>
2018-04-27 12:26:13 +0530

    saravanan reverese string added

--- src/testpack/BaseClass.java
+++ src/testpack/BaseClass.java
@@ -1,12 +1,13 @@
 package testpack;
 
-
+import java.util.Scanner;
 
 public class BaseClass {
 
 	public static void main(String[] args) {
 		BaseClass b=new BaseClass();
 		b.nagamani();
+		b.saravanan();
 
 	}
 	
@@ -29,6 +30,18 @@ public class BaseClass {
 		
 		
 	
+
+	}
+	
+	public void saravanan() {
+		String original, reverse = "";
+		Scanner s = new Scanner(System.in);
+		System.out.println("Enter a string to reverse");
+		original = s.nextLine();
+		int length = original.length();
+		for (int i = length - 1; i >= 0; i--)
+		reverse = reverse + original.charAt(i);
+		System.out.println("Reverse of entered string is: " + reverse);
 
 	}
 }
